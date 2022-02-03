418c1e418bfe7ebab29f0020246ee0510be927d8
tmhuyett <toddhuyett@gmail.com>
2022-02-03 17:20:21 -0500

    'd'

--- pages/recruiters.js
+++ pages/recruiters.js
@@ -1,13 +1,14 @@
 import useFetch from "../hooks/use-fetch"
-import AdminNav from '../components/AdminNav'
+import AdminNav from "../components/AdminNav"
+import formatDate from "../utils/format-date"
 import Link from "next/link"
 export default function Home() {
   const { data, loading, error } = useFetch(
     "https://radiant-oasis-73401.herokuapp.com/recruiters"
   )
-
   return (
-    <><AdminNav />
+    <>
+      <AdminNav />
       {loading && <>loading...</>}
       {error && <>there was an error...</>}
       {data && (
@@ -15,14 +16,17 @@ export default function Home() {
           {data.map(item => (
             <>
               <div id={data.indexOf(item) % 2 === 0 ? "evenBox" : "oddBox"}>
+                <div className="date">{formatDate(item.createdAt)}</div>
                 <div className="name">{item.name}</div>
                 <div className="company">{item.company}</div>
-           
-            
                 <div className="email">{item.email}</div>
                 <div className="phone">{item.phone}</div>
-                <div className="u"><a href={item.companyUrl}>{item.companyUrl}</a></div>
-                <div className="u"><a href={item.personalUrl}>{item.personalUrl}</a></div>
+                <div className="u">
+                  <a href={item.companyUrl}>{item.companyUrl}</a>
+                </div>
+                <div className="u">
+                  <a href={item.personalUrl}>{item.personalUrl}</a>
+                </div>
                 <p className="notes">{item.notes}</p>
                 <div className="edit">
                   <Link href={`/editrecruiter/${item._id}`}>
@@ -40,7 +44,6 @@ export default function Home() {
         }
         #oddBox {
           padding: 20px;
-          
         }
         .email {
           margin-top: 5px;
