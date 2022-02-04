45b62539cebe83959b498b83fd8f1f28736bd089
tmhuyett <toddhuyett@gmail.com>
2022-02-03 20:08:10 -0500

    'd'

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import PostList from "../components/PostList"
 import Layout from "../components/Layout"
+import AdminNav from '../components/AdminNav'
 
 export default function Home() {
   return (
     <>
+    <AdminNav />
       <Layout>
         <div id="offset">
           <PostList />
